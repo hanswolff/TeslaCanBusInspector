d7967732511f7a1923492a3ffb904177dac94cf1
Hans Wolff <hanswolff@users.noreply.github.com>
2018-07-01 17:39:51 +0200

    Implement BmsInfoMessage

---
+++ TeslaCanBusInspector.Tests/Models/BmsInfoMessageTests.cs
@@ -0,0 +1,31 @@
+﻿using FluentAssertions;
+using TeslaCanBusInspector.Models;
+using Xunit;
+
+namespace TeslaCanBusInspector.Tests.Models
+{
+    public class BmsInfoMessageTests
+    {
+        private readonly byte[] _examplePayload = new byte[] { 0x42, 0x22, 0xD1, 0x84 };
+
+        [Fact]
+        public void BatteryCurrent()
+        {
+            // Act      
+            var message = new BmsInfoMessage(_examplePayload);
+
+            // Assert
+            message.BmsMaxCharge.Should().Be(87.7m);
+        }
+        
+        [Fact]
+        public void BatteryVoltage()
+        {
+            // Act      
+            var message = new BmsInfoMessage(_examplePayload);
+
+            // Assert
+            message.BmsMaxDischarge.Should().Be(336.64m);
+        }
+    }
+}
---
+++ TeslaCanBusInspector/Models/BmsInfoMessage.cs
@@ -0,0 +1,30 @@
+﻿// ReSharper disable UnusedMember.Global
+namespace TeslaCanBusInspector.Models
+{
+    public class BmsInfoMessage : IBmsInfoMessage
+    {
+        public const ushort TypeId = 0x232;
+        public ushort MessageTypeId => TypeId;
+
+        public decimal BmsMaxCharge { get; }
+        public decimal BmsMaxDischarge { get; }
+
+        internal BmsInfoMessage()
+        {
+        }
+
+        public BmsInfoMessage(byte[] payload)
+        {
+            payload.RequireBytes(4);
+
+            BmsMaxCharge = (payload[0] + (payload[1] << 8)) / 100.0m;
+            BmsMaxDischarge = (payload[2] + (payload[3] << 8)) / 100.0m;
+        }
+    }
+
+    public interface IBmsInfoMessage : ICanBusMessage
+    {
+        decimal BmsMaxCharge { get; }
+        decimal BmsMaxDischarge { get; }
+    }
+}
