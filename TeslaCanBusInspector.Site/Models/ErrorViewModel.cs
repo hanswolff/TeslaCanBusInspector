c8122f13b7aaf4810473072b9fa1a1bf8c66b758
Hans Wolff <hanswolff@users.noreply.github.com>
2018-07-03 18:40:34 +0200

    Add MVC site project

---
+++ TeslaCanBusInspector.Site/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TeslaCanBusInspector.Site.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
