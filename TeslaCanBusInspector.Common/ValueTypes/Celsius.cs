b487c0386b6bfa92f9b9f470bceaa2f2752a1b71
Hans Wolff <hanswolff@users.noreply.github.com>
2018-07-05 17:34:51 +0200

    Add TemperatureMessage

---
+++ TeslaCanBusInspector.Common/ValueTypes/Celsius.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace TeslaCanBusInspector.Common.ValueTypes
+{
+    [SuppressMessage("ReSharper", "UnusedMember.Global")]
+    public struct Celsius : IEquatable<Celsius>
+    {
+        public readonly decimal Value;
+
+        public Celsius(int value)
+        {
+            Value = value;
+        }
+
+        public Celsius(decimal value)
+        {
+            Value = value;
+        }
+
+        public Celsius(double value)
+        {
+            Value = (decimal)value;
+        }
+
+        public Celsius(float value)
+        {
+            Value = (decimal)value;
+        }
+
+        public bool Equals(Celsius other)
+        {
+            return Value == other.Value;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            return obj is Celsius && Equals((Celsius)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return Value.GetHashCode();
+        }
+
+        public static implicit operator decimal(Celsius valueType)
+        {
+            return valueType.Value;
+        }
+
+        public override string ToString()
+        {
+            return $"{Value:N2} °C";
+        }
+
+        public string ToString(IFormatProvider formatProvider)
+        {
+            return Value.ToString(formatProvider);
+        }
+
+        public string ToString(string format)
+        {
+            return Value.ToString(format);
+        }
+
+        public string ToString(string format, IFormatProvider formatProvider)
+        {
+            return Value.ToString(format, formatProvider);
+        }
+    }
+}
